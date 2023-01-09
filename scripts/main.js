9603dd404abfb498bbe6a9798fdc479471fae112
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-01-09 14:29:37 +0200

    suvarkyta kad pradingtu meniu

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,22 @@
+const header = document.querySelector('header');
 
-document.querySelectorAll("nav a").forEach(button=> button.addEventListener("click", e=>{
-  e.preventDefault();
+document.querySelectorAll("nav a").forEach(button => {
+  button.addEventListener("click", e => {
+    e.preventDefault();
 
-  let href = e.target.getAttribute("href");
-  let section = document.getElementById(href);
-  let closeButton = section.childNodes[1].childNodes[1];
+    let href = e.target.getAttribute("href");
+    let section = document.getElementById(href);
+    let closeButton = section.childNodes[1].childNodes[1];
 
-  if(section) {
-    section.classList.toggle("show");
-    closeButton.addEventListener('click', e=>{
-      e.target.parentNode.parentNode.classList.toggle("show")
-    }, { once:true });
-  }
-}));
+    header.style.display = 'none';
+
+    if (section) {
+      section.classList.toggle("show");
+      closeButton.addEventListener('click', e => {
+        e.target.parentNode.parentNode.classList.toggle("show");
+ 
+        header.style.display = 'flex';
+      }, { once: true });
+    }
+  });
+});
