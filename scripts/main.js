4ed2f164ec9c2a86fc9d653e9f5d954dcc679dca
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-01-09 14:15:30 +0200

    patavrkyta korteliu isvaizda

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,12 @@ document.querySelectorAll("nav a").forEach(button=> button.addEventListener("cli
 
   let href = e.target.getAttribute("href");
   let section = document.getElementById(href);
+  let closeButton = section.childNodes[1].childNodes[1];
+
   if(section) {
     section.classList.toggle("show");
-    section.childNodes[1].childNodes[1].addEventListener('click', e=>{
+    closeButton.addEventListener('click', e=>{
       e.target.parentNode.parentNode.classList.toggle("show")
-    });
+    }, { once:true });
   }
 }));
